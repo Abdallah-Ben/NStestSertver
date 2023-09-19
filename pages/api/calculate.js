1d486b004464766f3aee66359f6164c7f46d73a7
Abdallah-Ben <abdallah_ben@me.com>
2023-09-19 22:19:40 +0100

    first commit

---
+++ pages/api/calculate.js
@@ -0,0 +1,27 @@
+export default async function calculate(req, res) {
+  const { operator, num1, num2 } = req.body
+
+  let result
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    switch (operator) {
+      case '+':
+        result = num1 + num2
+        break
+      case '-':
+        result = num1 - num2
+        break
+      case '*':
+        result = num1 * num2
+        break
+      case '/':
+        result = num1 / num2
+        break
+      default:
+        result = 'Invalid operator'
+    }
+  } else {
+    result = 'Invalid numbers.'
+  }
+
+  res.json({ result })
+}
